ac975069d33fce4295eab0724bfd41f2b665458f
OlenaChupryna <chupryna.ol@gmail.com>
2019-05-04 16:53:14 +0300

    image fixing

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  url = "./assets/images/checkmark.svg";
+
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 
 }
